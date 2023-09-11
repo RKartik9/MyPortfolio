b5bdbba042f79934900a1971a1c47e0f09a8f0f6
Rohit Kumar <urskartik9@gmail.com>
2023-09-11 17:51:05 +0530

    changed resume

--- pages/index.js
+++ pages/index.js
@@ -103,7 +103,7 @@ export default function Home() {
                     className=" bg-gradient-to-r from bg-cyan-500 to-teal-500 text-white
                     px-1
                   lg:px-2 py-2 rounded-md ml-3 lg:ml-8 text-0.5rem "
-                    href="https://drive.google.com/file/d/1jKfyFMaRLNEJXjXU8VMSY7fhz8Wq5dEt/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1atO00fzOpvHOnSlJp8kjO-SfOBT1_3-0/view?usp=sharing"
                     target="_blank"
                   >
                     Resume
